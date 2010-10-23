cb3e527dab02976b80ba017e9d96c48ce10edf11
gedex.adc <gedex.adc@01bd070f-5a84-190f-e431-89738f2bf04b>
2010-10-23 10:10:41 +0000

    add callback to force dependent records on checkups_medicine

--- models/medicine.php
+++ models/medicine.php
@@ -3,5 +3,21 @@ class Medicine extends AppModel {
     var $belongsTo = array(
         'Unit'
     );
+    
+    function afterDelete() {
+        $this->bindModel(array(
+            'hasMany' => array(
+                'CheckupsMedicine' => array(
+                    'className' => 'CheckupsMedicine'
+                )
+            )
+        ));
+        
+        $this->CheckupsMedicine->deleteAll(
+            array('CheckupsMedicine.medicine_id' => $this->id)
+        );
+        
+        return true;
+    }
 }
 ?>
